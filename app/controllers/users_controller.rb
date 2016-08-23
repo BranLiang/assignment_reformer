acc7c2de496ee32d5f3b54521ce8bfcee81b291e
BranLiang <lby89757@hotmail.com>
2016-08-23 11:58:33 +0800

    finish the assignment

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,25 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
+    # @user = User.new( :username => params[:username],
+    #                   :email => params[:email],
+    #                   :password => params[:password] )
+    @user = User.new(white_list_params)
+    if @user.save
+      redirect_to new_user_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @user = User.find(:id)
+  end
+
+  def white_list_params
+    params.require(:user).permit(:username, :email, :password)
   end
 end
